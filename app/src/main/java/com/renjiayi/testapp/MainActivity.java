3d7d32d2a700eba680358cb994957437988b3514
renjiayi <1020283848@qq.com>
2019-10-07 22:31:38 +0800

    Test

--- app/src/main/java/com/renjiayi/testapp/MainActivity.java
+++ app/src/main/java/com/renjiayi/testapp/MainActivity.java
@@ -2,13 +2,17 @@ package com.renjiayi.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button button = findViewById(R.id.ssssss);
+        button.setTextColor(Color.RED);
         setContentView(R.layout.activity_main);
     }
 }
